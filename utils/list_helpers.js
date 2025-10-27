7d887fb5e76c2531d4b489aaf158700e7e88a5ca
Fernando <8602500+45Fernando@users.noreply.github.com>
2025-10-27 20:08:34 -0300

    part 4.4

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((acc, blog) => {
+    return acc + blog.likes;
+  }, 0);
+
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
