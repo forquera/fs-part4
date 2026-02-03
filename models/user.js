5e9614016ba2a38fe077660833ce74887bdd70c7
Fernando <8602500+45Fernando@users.noreply.github.com>
2026-02-02 21:49:56 -0300

    part 4.16

--- models/user.js
+++ models/user.js
@@ -5,9 +5,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    minlength: 3,
   },
   name: String,
-  passwordHash: String,
+  passwordHash: { type: String, required: true },
 });
 
 userSchema.set("toJSON", {
